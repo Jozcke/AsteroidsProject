a45fd3be8940aeeeb1c71ba67efe23146e905921
Joakim Olsson <18533720+Jozcke@users.noreply.github.com>
2025-12-09 22:01:11 +0100

    painchamp

--- Asteroids/Asteroids/src/main.cpp
+++ Asteroids/Asteroids/src/main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
-
+#include "SFML/graphics.hpp"
 
 
 int main() {
 
+	sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "SFML window");
 
-
-
-
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        while (const std::optional event = window.pollEvent())
+        {
+            // "close requested" event: we close the window
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+    }
 	return 0;
 }
