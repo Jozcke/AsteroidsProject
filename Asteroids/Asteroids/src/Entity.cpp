392d82eabf0187f7735cfb6e0ced1d0865956261
Joakim Olsson <18533720+Jozcke@users.noreply.github.com>
2026-01-13 17:13:21 +0100

    improved screen wrapping.

--- Asteroids/Asteroids/src/Entity.cpp
+++ Asteroids/Asteroids/src/Entity.cpp
@@ -1,21 +1,5 @@
 #include "Entity.h"
 
-
-void Entity::wrapAroundScreen(const sf::RenderWindow& window)
-{
-    sf::Vector2u size = window.getSize();
-
-    if (position.x < 0.f)
-        position.x = static_cast<float>(size.x);
-    else if (position.x > size.x)
-        position.x = 0.f;
-
-    if (position.y < 0.f)
-        position.y = static_cast<float>(size.y);
-    else if (position.y > size.y)
-        position.y = 0.f;
-}
-
 void Entity::setAlive(bool alive)
 {
     this->alive = alive;
@@ -34,4 +18,19 @@ void Entity::setPosition(const sf::Vector2f& pos)
 sf::Vector2f Entity::getPosition() const
 {
     return this->position;
+}
+
+void Entity::wrapAroundScreen(const sf::RenderWindow& window)
+{
+    sf::Vector2u size = window.getSize();
+
+    if (position.x < -getRadius())
+        position.x = size.x + getRadius();
+    else if (position.x > size.x + getRadius())
+        position.x = -getRadius();
+
+    if (position.y < -getRadius())
+        position.y = size.y + getRadius();
+    else if (position.y > size.y + getRadius())
+        position.y = -getRadius();
 }
